a3884943d15bab1e94bbeca8e29a921ad414cd70
tomantjon <tom.antjon@gmail.com>
2019-04-16 15:21:15 +0200

    removed unnecessary chars

--- promise/js/promiseGok1.js
+++ promise/js/promiseGok1.js
@@ -1,11 +1,11 @@
-const promiseGok1 = new Promise((resolve, reject) => {   
-    const gok = Number.parseInt(prompt("Geef een getal tussen 1 en 3"));
-    const randomWaarde = Math.floor(Math.random() * 3) + 1;
-    gok === randomWaarde ? resolve() : reject(randomWaarde);
+const promiseGok1 = new Promise((resolve, reject) => {
+  const gok = Number.parseInt(prompt('Geef een getal tussen 1 en 3'));
+  const randomWaarde = Math.floor(Math.random() * 3) + 1;
+  gok === randomWaarde ? resolve() : reject(randomWaarde);
 })
-.then(() => {
-    console.log("Juist gegokt! U hebt een haardroger gewonnen!");
-})
-.catch((rejectWaarde) => {
+  .then(() => {
+    console.log('Juist gegokt! U hebt een haardroger gewonnen!');
+  })
+  .catch(rejectWaarde => {
     console.log(`Fout gegokt! De waarde op de dobbelsteen was ${rejectWaarde}`);
-});
+  });
